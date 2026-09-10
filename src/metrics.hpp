36efdccdcc6288ca896c118eb8588a95de97b79c
Ben Baker <bakerb845@gmail.com>
2026-09-10 13:29:46 -0600

    beginning to add metrics

--- src/metrics.hpp
+++ src/metrics.hpp
@@ -13,6 +13,7 @@
 #include <opentelemetry/exporters/otlp/otlp_grpc_metric_exporter_factory.h>
 #include <opentelemetry/exporters/otlp/otlp_grpc_metric_exporter_options.h>
 #endif
+#include <opentelemetry/metrics/provider.h>
 #include <opentelemetry/sdk/metrics/export/periodic_exporting_metric_reader_factory.h>
 #include <opentelemetry/sdk/metrics/export/periodic_exporting_metric_reader_options.h>
 #include <opentelemetry/sdk/metrics/meter_context.h>
@@ -28,6 +29,20 @@ namespace
 {
 
 bool metricsInitialized{false};
+
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument>
+    successCounter{nullptr};
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument>
+    clientErrorCounter{nullptr};
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument>
+    serverErrorCounter{nullptr};
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument>
+    unauthorizedCounter{nullptr};
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument>
+    unauthenticatedCounter{nullptr};
+opentelemetry::nostd::unique_ptr<opentelemetry::metrics::Histogram<double>>
+    routePerformanceHistogram{nullptr};
+
 
 void initializeRouteHistogram(
     opentelemetry::sdk::metrics::MeterProvider *providerInstance)
@@ -167,7 +182,169 @@ void initializeGRPC(
 }
 #endif
 
+/// Send 200s for routes
+void observeSuccessCounts(
+    opentelemetry::metrics::ObserverResult observerResult,
+    void *)
+{
+    if (opentelemetry::nostd::holds_alternative
+        <
+            opentelemetry::nostd::shared_ptr
+            <
+                opentelemetry::metrics::ObserverResultT<double>
+            >
+        > (observerResult))
+    {
+        auto observer = opentelemetry::nostd::get
+        <
+            opentelemetry::nostd::shared_ptr
+            <
+               opentelemetry::metrics::ObserverResultT<double>
+            >
+        > (observerResult);
+        // N.B. these should all be noexcepts
+        auto &instance
+            = AQMSDutyReviewBackend::Metrics::MetricsSingleton::getInstance();
+        auto map = instance.getSuccessCounters();
+        for (const auto &item : map)
+        {
+            auto key = item.first;
+            auto value = item.second;
+            std::map<std::string, std::string>
+                attribute{ {"route", item.first} };
+            observer->Observe(value, attribute);
+        }
+    }
 }
+
+/// Send 400s for routes
+void observeClientErrorCounts(
+    opentelemetry::metrics::ObserverResult observerResult,
+    void *)
+{
+    if (opentelemetry::nostd::holds_alternative
+        <   
+            opentelemetry::nostd::shared_ptr
+            <   
+                opentelemetry::metrics::ObserverResultT<double>
+            >   
+        > (observerResult))
+    {
+        auto observer = opentelemetry::nostd::get
+        <   
+            opentelemetry::nostd::shared_ptr
+            <   
+               opentelemetry::metrics::ObserverResultT<double>
+            >   
+        > (observerResult);
+        // N.B. these should all be noexcepts
+        auto &instance
+            = AQMSDutyReviewBackend::Metrics::MetricsSingleton::getInstance();
+        auto map = instance.getClientErrorCounters();
+        for (const auto &item : map)
+        {
+            auto key = item.first;
+            auto value = item.second;
+            std::map<std::string, std::string>
+                attribute{ {"route", item.first} };
+            observer->Observe(value, attribute);
+        }
+    }   
+}
+
+/// Send 500s for routes
+void observeServerErrorCounts(
+    opentelemetry::metrics::ObserverResult observerResult,
+    void *)
+{
+    if (opentelemetry::nostd::holds_alternative
+        <   
+            opentelemetry::nostd::shared_ptr
+            <   
+                opentelemetry::metrics::ObserverResultT<double>
+            >   
+        > (observerResult))
+    {
+        auto observer = opentelemetry::nostd::get
+        <   
+            opentelemetry::nostd::shared_ptr
+            <   
+               opentelemetry::metrics::ObserverResultT<double>
+            >   
+        > (observerResult);
+        // N.B. these should all be noexcepts
+        auto &instance
+            = AQMSDutyReviewBackend::Metrics::MetricsSingleton::getInstance();
+        auto map = instance.getServerErrorCounters();
+        for (const auto &item : map)
+        {
+            auto key = item.first;
+            auto value = item.second;
+            std::map<std::string, std::string>
+                attribute{ {"route", item.first} };
+            observer->Observe(value, attribute);
+        }
+    }   
+}
+
+/// 401 errors
+void observeUnauthenticatedCounts(
+    opentelemetry::metrics::ObserverResult observerResult,
+    void *)
+{
+    if (opentelemetry::nostd::holds_alternative
+        <
+            opentelemetry::nostd::shared_ptr
+            <
+                opentelemetry::metrics::ObserverResultT<double>
+            >
+        > (observerResult))
+    {
+        auto observer = opentelemetry::nostd::get
+        <
+            opentelemetry::nostd::shared_ptr
+            <
+               opentelemetry::metrics::ObserverResultT<double>
+            >
+        > (observerResult);
+        // N.B. these should all be noexcepts
+        auto &instance
+            = AQMSDutyReviewBackend::Metrics::MetricsSingleton::getInstance();
+        observer->Observe(instance.getUnauthenticatedCounts());
+    }
+}
+
+/// 403 errors
+void observeUnauthorizedCounts(
+    opentelemetry::metrics::ObserverResult observerResult,
+    void *)
+{
+    if (opentelemetry::nostd::holds_alternative
+        <
+            opentelemetry::nostd::shared_ptr
+            <
+                opentelemetry::metrics::ObserverResultT<double>
+            >
+        > (observerResult))
+    {   
+        auto observer = opentelemetry::nostd::get
+        <
+            opentelemetry::nostd::shared_ptr
+            <
+               opentelemetry::metrics::ObserverResultT<double>
+            >
+        > (observerResult);
+        // N.B. these should all be noexcepts
+        auto &instance
+            = AQMSDutyReviewBackend::Metrics::MetricsSingleton::getInstance();
+        observer->Observe(instance.getUnauthorizedCounts());
+    }   
+}
+
+
+}
+
+///-------------------------------------------------------------------------///
 
 namespace AQMSDutyReviewBackend::Metrics
 {
@@ -192,6 +369,66 @@ void initialize(
     }   
 }
 
+void createMeters(const ProgramOptions &options)
+{
+    if (!options.exportMetrics){return;}
+
+    auto provider
+        = opentelemetry::metrics::Provider::GetMeterProvider();
+    auto meter = provider->GetMeter(options.applicationName, "1.2.0");
+
+    // 200 counts (all routes)
+    successCounter
+        = meter->CreateInt64ObservableCounter(
+             "aqms.drp.responses.success",
+             "Number of 200 responses to user.",
+             nullptr); 
+    successCounter->AddCallback(::observeSuccessCounts, nullptr);
+
+    // 400 counts (all routes)
+    clientErrorCounter
+        = meter->CreateInt64ObservableCounter(
+             "aqms.drp.responses.errors.client",
+             "Number of 40x responses to user.",
+             nullptr); 
+    clientErrorCounter->AddCallback(::observeClientErrorCounts, nullptr);
+
+    // 500 counts (all routes)
+    serverErrorCounter
+        = meter->CreateInt64ObservableCounter(
+             "aqms.drp.responses.errors.server",
+             "Number of 50x responses to user.",
+             nullptr);
+    serverErrorCounter->AddCallback(::observeServerErrorCounts, nullptr);
+
+    // 401 counts
+    unauthenticatedCounter
+        = meter->CreateInt64ObservableCounter(
+             "aqms.drp.responses.unauthenticated",
+             "Number of 401 Unauthorized responses to user.",
+             nullptr); 
+    unauthenticatedCounter->AddCallback(
+        ::observeUnauthenticatedCounts, nullptr);
+
+    // 403 counts (I know who you are but you don't have permission)
+    unauthorizedCounter
+        = meter->CreateInt64ObservableCounter(
+             "aqms.drp.responses.unauthorized",
+             "Number of 403 Forbidden responses to user.",
+             nullptr);
+    unauthorizedCounter->AddCallback(
+        ::observeUnauthorizedCounts, nullptr);
+}
+
+void recordRequestDuration(const std::chrono::nanoseconds &duration,
+                           const std::string &route)
+{
+    auto durationSeconds = static_cast<double> (duration.count())*1.e-9;
+    // TODO do i need a context auto otelContext = opentelemetry::context::Context {};
+    std::map<std::string, std::string> histogramKey{ {"route", route} };
+    routePerformanceHistogram->Record(durationSeconds, histogramKey);
+}
+
 void cleanup()
 {
     if (metricsInitialized)
@@ -204,3 +441,5 @@ void cleanup()
 
 }
 #endif
+
+
