6dc8eca39e84354fb3f2c0ae33fa040af5f2f82d
Ben Baker <bakerb845@gmail.com>
2026-09-10 11:42:15 -0600

    adding more metrics

--- include/aqmsDutyReviewBackend/metricsSingleton.hpp
+++ include/aqmsDutyReviewBackend/metricsSingleton.hpp
@@ -20,6 +20,33 @@ public:
     /// @result An instance of the singleton.
     [[maybe_unused]] static MetricsSingleton &getInstance();
 
+    /// @brief Increments the 500 error for this route.
+    void incrementServerErrorCounter(const std::string &route);
+    /// @result The error counts for the utilized routes.
+    [[nodiscard]] std::map<std::string, int64_t> getServerErrorCounters() const; 
+
+    /// @brief Increments the 400 error for this route.
+    void incrementClientErrorCounter(const std::string &route);
+    /// @result The client error counts for the utilized routes.
+    [[nodiscard]] std::map<std::string, int64_t> getClientErrorCounters() const;
+
+    /// @brief Whenever a user hits a route and is unauthorized this
+    ///        gets incremented.
+    void incrementUnauthorizedCounter() noexcept;
+    /// @result The number of unauthenticated attempts.
+    [[nodiscard]] int64_t getUnauthorizedCounts() const noexcept;
+
+    /// @brief Whenever a user presents invalid credentials this
+    ///        gets incremented.
+    void incrementUnauthenticatedCounter() noexcept;
+    /// @result The number of unauthenticated attempts.
+    [[nodiscard]] int64_t getUnauthenticatedCounts() const noexcept;
+
+    ///  @brief Increments the 200 successes for this route.
+    void incrementSuccessCounter(const std::string &route);
+    /// @result The success counts for the utilized routes.
+    [[nodiscard]] std::map<std::string, int64_t> getSuccessCounters() const;
+
     /// @brief Resets the counters an dutilization.  This is useful for unit tests.
     void resetMetrics() noexcept;
 private:
@@ -27,9 +54,11 @@ private:
     ~MetricsSingleton() = default;
     mutable std::mutex mMutex;
     // The following maps use the form (route, count) (e.g., (auth-login, 1):
-    std::map<std::string, int64_t> mServerErrorCounter; // 500 response codes
-    std::map<std::string, int64_t> mClientErrorCounter; // 400 response codes
-    std::map<std::string, int64_t> mSuccessCounter;     // 200 response codes
+    std::map<std::string, int64_t> mServerErrorCounterMap; // 500 response codes
+    std::map<std::string, int64_t> mClientErrorCounterMap; // 400 response codes
+    std::map<std::string, int64_t> mSuccessCounterMap;     // 200 response codes
+    std::atomic<int64_t> mUnauthorizedCounter{0};
+    std::atomic<int64_t> mUnauthenticatedCounter{0};
 };
 
 /// @brief Initializes the metrics singleton once and for all.  This is to be
--- src/metricsSingleton.cpp
+++ src/metricsSingleton.cpp
@@ -1,4 +1,8 @@
+#include <atomic>
+#include <cstdint>
+#include <map>
 #include <mutex>
+#include <string>
 #include "aqmsDutyReviewBackend/metricsSingleton.hpp"
 
 using namespace AQMSDutyReviewBackend::Metrics;
@@ -9,19 +13,113 @@ MetricsSingleton &MetricsSingleton::getInstance()
     return instance;
 }
 
+/// Server error counts 
+void MetricsSingleton::incrementServerErrorCounter(
+    const std::string &route)
+{
+    if (route.empty()){return;}
+    {
+    const std::scoped_lock lock{mMutex};
+    auto it = mServerErrorCounterMap.find(route);
+    if (it != mServerErrorCounterMap.end())
+    {
+        it->second = it->second + 1;
+        return;
+    }
+    mServerErrorCounterMap.insert( {route, 1} );
+    }
+}
+
+std::map<std::string, int64_t> MetricsSingleton::getServerErrorCounters() const
+{
+    const std::scoped_lock lock{mMutex};
+    return mServerErrorCounterMap;
+}
+
+/// Client error counts 
+void MetricsSingleton::incrementClientErrorCounter(
+    const std::string &route)
+{
+    if (route.empty()){return;}
+    {
+    const std::scoped_lock lock{mMutex};
+    auto it = mClientErrorCounterMap.find(route);
+    if (it != mClientErrorCounterMap.end())
+    {
+        it->second = it->second + 1;
+        return;
+    }
+    mClientErrorCounterMap.insert( {route, 1} );
+    }
+}
+
+std::map<std::string, int64_t> MetricsSingleton::getClientErrorCounters() const
+{
+    const std::scoped_lock lock{mMutex};
+    return mClientErrorCounterMap;
+}
+
+
+/// Success counts 
+void MetricsSingleton::incrementSuccessCounter(
+    const std::string &route)
+{
+    if (route.empty()){return;}
+    {   
+    const std::scoped_lock lock{mMutex};
+    auto it = mSuccessCounterMap.find(route);
+    if (it != mSuccessCounterMap.end())
+    {   
+        it->second = it->second + 1;
+        return;
+    }   
+    mSuccessCounterMap.insert( {route, 1} );
+    }   
+}
+
+std::map<std::string, int64_t> MetricsSingleton::getSuccessCounters() const
+{
+    const std::scoped_lock lock{mMutex};
+    return mSuccessCounterMap;
+}
+
+/// Unauthorized 
+void MetricsSingleton::incrementUnauthorizedCounter() noexcept
+{
+    mUnauthorizedCounter.fetch_add(1, std::memory_order_relaxed);        
+}
+
+int64_t MetricsSingleton::getUnauthorizedCounts() const noexcept
+{
+    return mUnauthorizedCounter.load(std::memory_order_relaxed);
+}
+
+/// Unauthenticated
+void MetricsSingleton::incrementUnauthenticatedCounter() noexcept
+{
+    mUnauthenticatedCounter.fetch_add(1, std::memory_order_relaxed);
+}
+
+int64_t MetricsSingleton::getUnauthenticatedCounts() const noexcept
+{
+    return mUnauthenticatedCounter.load(std::memory_order_relaxed);
+}
+
 /// Reset
 void MetricsSingleton::resetMetrics() noexcept
 {
+    mUnauthorizedCounter.store(0);
+    mUnauthenticatedCounter.store(0);
     const std::scoped_lock lock{mMutex}; 
-    for (auto &counter : mServerErrorCounter)
+    for (auto &counter : mServerErrorCounterMap)
     {
         counter.second = 0;
     }
-    for (auto &counter : mClientErrorCounter)
+    for (auto &counter : mClientErrorCounterMap)
     {
         counter.second = 0;
     }
-    for (auto &counter : mSuccessCounter)
+    for (auto &counter : mSuccessCounterMap)
     {
         counter.second = 0;
     }
